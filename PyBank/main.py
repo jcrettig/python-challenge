fc1cfd4f280efd2bd62cef8685db3dcc38da542e
Chris Rettig <Chrisr@impa.com>
2020-12-20 16:04:05 -0500

    Created Month and Profit_Loss Lists in PyBank code

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,10 +6,26 @@ import csv
 # Assumes running from following path: C:\Chris\but-ind-data-pt-12-2020-u-c\Week-03-Python\Homework\python-challenge>
 csvpath = os.path.join('PyBank','Resources', 'budget_data.csv')
 
+#Define lists
+months = []
+profit_losses = []
+
 #Read using CSV module
 with open(csvpath) as csvfile:
 
     #Specify delimiter and variable that holds contents
-    csvreader = csv.reader(csvfile,delimiter=',')
+    csv_reader = csv.reader(csvfile,delimiter=',')
 
-    print(csvreader)
+    #Read header row but exclude from lists
+    next(csv_reader)
+
+    #Read each row of data and insert it into the respective list
+    for row in csv_reader:
+        months.append(row[0])    
+        profit_losses.append(row[1])
+    
+print(months)
+print(profit_losses)
+
+
+
